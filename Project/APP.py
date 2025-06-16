417729ddc52f4c25a3cfa14250a74e7b95e95269
MUPPIDI VENKATA BALAJI 22BCB7289 <venkata.22bcb7289@vitapstudent.ac.in>
2025-06-16 20:08:39 +0530

    Update APP.py

--- Project/APP.py
+++ Project/APP.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-client = MongoClient(client = MongoClient("mongodb+srv://Balaji:22BCB7289@cluster0.2avlt7z.mongodb.net/")
+client = MongoClient("mongodb+srv://Balaji:22BCB7289@cluster0.2avlt7z.mongodb.net/Menu?retryWrites=true&w=majority")
 db = client["Menu"]
 foods = db["foods"]
 
