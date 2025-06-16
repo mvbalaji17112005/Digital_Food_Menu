401132335f6bca24a69700f44c3935448df05eec
MUPPIDI VENKATA BALAJI 22BCB7289 <venkata.22bcb7289@vitapstudent.ac.in>
2025-06-16 21:36:53 +0530

    Update APP.py

--- Project/APP.py
+++ Project/APP.py
@@ -56,5 +56,5 @@ def delete_food(food_id):
     return redirect(url_for('index', msg="deleted"))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
